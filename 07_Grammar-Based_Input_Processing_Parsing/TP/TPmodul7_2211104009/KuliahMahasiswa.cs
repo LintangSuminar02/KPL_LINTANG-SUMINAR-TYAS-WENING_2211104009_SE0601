64dad070ff8d118630bf603c3ecb1074b9c860d7
Marianaferaz <nathasyamaria80@gmail.com>
2025-06-12 20:09:00 +0700

    "menambahkan class KuliahMahasiswa"

---
+++ 07_Grammar-Based_Input_Processing_Parsing/TP/TPmodul7_2211104009/KuliahMahasiswa.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPmodul7_2211104009
+{
+    class KuliahMahasiswa
+    {
+        public class Course
+        {
+            public string Code { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class CourseList
+        {
+            public List<Course> Courses { get; set; }
+        }
+
+        public static void ReadJSON()
+        {
+            string filePath = "tp7_2_2211104009.json";
+
+            // Cek apakah file JSON ada
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("File JSON tidak ditemukan.");
+                return;
+            }
+
+            // Membaca isi file JSON
+            string jsonContent = File.ReadAllText(filePath);
+
+            // Melakukan deserialisasi JSON ke objek
+            CourseList data = JsonConvert.DeserializeObject<CourseList>(jsonContent);
+
+            // Menampilkan hasil
+            Console.WriteLine("Daftar mata kuliah yang diambil:");
+            int index = 1;
+            foreach (var course in data.Courses)
+            {
+                Console.WriteLine($"MK {index} {course.Code} - {course.Name}");
+                index++;
+            }
+        }
+    }
+}
